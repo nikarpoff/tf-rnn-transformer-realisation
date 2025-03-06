fix: fix GRU computation and LSTM output

main.py
@@ -36,8 +36,8 @@ TRANSFORMER_MODELS_PATH = os.path.join(MODELS_PATH, "transformer_translators")
 TRANSFORMER_RU_MODEL_PATH = os.path.join(RNN_MODELS_PATH, "transformer-ru-to-en-translator")
 TRANSFORMER_EN_MODEL_PATH = os.path.join(RNN_MODELS_PATH, "transformer-en-to-ru-translator")
 
-TOKEN_LENGTH = 300
-SEQUENCE_SIZE = 15
+TOKEN_LENGTH = 100
+SEQUENCE_SIZE = 10
 LEARNING_RATE = 0.001
 BATCH_SIZE = 64
 EPOCHS = 20
@@ -117,7 +117,8 @@ def fit_vectorization_models(dataset, token_length):
 def fit_rnn(train_data, val_data, optimizer, loss, encoder_units, token_length, max_sequence_size, save_path, seed):
     # Initialize model.
     translator = TranslatorRNN(encoder_units=encoder_units, token_length=token_length, max_sequence_size=max_sequence_size, seed=seed)
-    print(translator)
+    translator.build(input_shape=(BATCH_SIZE, max_sequence_size, token_length))
+    translator.summary()
 
     # Fit model.
     translator.compile(optimizer=optimizer, loss=loss)
@@ -180,7 +181,7 @@ def main():
 
     # Specify optimizer and loss function.
     optimizer = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
-    loss = tf.keras.losses.MeanSquaredError()    
+    loss = tf.keras.losses.CosineSimilarity()    
         
     if task == "test":
         if model == "rnn":
@@ -193,8 +194,8 @@ def main():
     # There we have "train" task
     if task == "train":
         if model == "rnn":
-            fit_rnn(train_data, val_data, optimizer=optimizer, loss=loss, encoder_units=[500, 500],
-                    token_length=TOKEN_LENGTH, max_sequence_size=SEQUENCE_SIZE, save_path=rnn_path, seed=7)
+            fit_rnn(train_data, val_data, optimizer=optimizer, loss=loss, encoder_units=[500, 1000],
+                    token_length=TOKEN_LENGTH, max_sequence_size=SEQUENCE_SIZE, save_path=rnn_path, seed=42)
         else:
             print("TODO...")
     else:

rnn/demo.py
@@ -16,7 +16,7 @@ import tensorflow as tf
 
 from translator import TranslatorRNN
 from rnn.lstm import DecoderLSTM, CellLSTM
-from rnn.gru import DeepEncoderGRU, GRU, CellGRU
+from rnn.gru import DeepEncoderGRU, EncoderGRU, CellGRU
 from util.preprocess import TextPreprocessing
 
 
@@ -24,7 +24,7 @@ def translate_with_rnn(string: str, preprocessing: TextPreprocessing, model_path
     try:
         loaded_model = tf.keras.models.load_model(model_path, custom_objects={"TranslatorRNN": TranslatorRNN,
                                                                               "DeepEncoderGRU": DeepEncoderGRU,
-                                                                              "GRU": GRU,
+                                                                              "GRU": EncoderGRU,
                                                                               "CellGRU": CellGRU,
                                                                               "DecoderLSTM": DecoderLSTM,
                                                                               "CellLSTM": CellLSTM})

rnn/gru.py
@@ -70,7 +70,7 @@ class CellGRU(tf.keras.layers.Layer):
         }
 
 
-class GRU(tf.keras.layers.Layer):
+class EncoderGRU(tf.keras.layers.Layer):
     def __init__(self, units: int, token_length: int, use_bias: bool, return_sequences: bool, seed: int, name=None):
         super().__init__(name=name)
         self.units = units
@@ -84,18 +84,19 @@ class GRU(tf.keras.layers.Layer):
         batch_size = tf.shape(x)[0]
         time_steps = tf.shape(x)[1]
 
-        # The one step of LSTM computation.
+        # The one step of GRU computation.
         def loop_body(step, h, hidden_states):
             x_t = x[:, step, :]
             h_next = self.cell(x_t, h)  # compute new h
-            hidden_states = hidden_states.write(step, h_next)  # remember h
-            return step + 1, h, hidden_states
-        
-        # Initialize h_0.
-        h = tf.zeros((batch_size, self.units), dtype=tf.float32)
-        hidden_states = tf.TensorArray(tf.float32, size=time_steps)  # list of (batch_size, units) outputs with sequence_length size
-        
-        # Every element of sequence is x on one step.
+            h_next.set_shape([None, self.units])  # h shape is constant
+            hidden_states = hidden_states.write(step, h_next)  # store h at current step
+            return step + 1, h_next, hidden_states
+
+        # Initialize h_0 and array for all of h.
+        h = tf.zeros(shape=(batch_size, self.units), dtype=tf.float32)
+        hidden_states = tf.TensorArray(dtype=tf.float32, size=time_steps)
+
+        # Run loop with shape invariants
         _, h, hidden_states = tf.while_loop(
             cond=lambda step, *_: step < time_steps,
             body=loop_body,
@@ -103,9 +104,8 @@ class GRU(tf.keras.layers.Layer):
         )
 
         if self.return_sequences:
-            # Transform list to tensor (stacked by batches).
-            return tf.transpose(hidden_states.stack(), [1, 0, 2])  # (batch_size, seq_len, hidden_dim)
-
+            # Stack hidden states to (seq_len, batch_size, units) and transpose
+            return tf.transpose(hidden_states.stack(), [1, 0, 2])
         return h
 
     def __str__(self):
@@ -141,7 +141,7 @@ class DeepEncoderGRU(tf.keras.layers.Layer):
         
         # First layer has input shape (batch_size, seq_len, token_length).
         self.deep_model = [
-            GRU(
+            EncoderGRU(
                 units=units_list[0],
                 token_length=token_length,
                 use_bias=use_bias,
@@ -152,7 +152,7 @@ class DeepEncoderGRU(tf.keras.layers.Layer):
 
         # Another user specified layers have input shape (batch_size, seq_len, units_prev_layer).
         for i in range(1, len(units_list) - 1):
-            self.deep_model.append(GRU(
+            self.deep_model.append(EncoderGRU(
                 units=units_list[i],
                 token_length=units_list[i - 1],
                 use_bias=use_bias,
@@ -161,7 +161,7 @@ class DeepEncoderGRU(tf.keras.layers.Layer):
             ))
 
         # Last layer can not return sequences.
-        self.deep_model.append(GRU(
+        self.deep_model.append(EncoderGRU(
             units=units_list[-1],
             token_length=units_list[-2],
             use_bias=use_bias,

rnn/lstm.py
@@ -93,12 +93,12 @@ class DecoderLSTM(tf.keras.layers.Layer):
         self.dense = tf.keras.Sequential([
             tf.keras.layers.Dense(
                 input_length,
-                activation='relu',
+                activation='tanh',
                 kernel_initializer=tf.keras.initializers.GlorotUniform(seed=seed),
                 use_bias=use_bias
         ), tf.keras.layers.Dense(
                 output_token_length,
-                activation='tanh',
+                activation='linear',
                 kernel_initializer=tf.keras.initializers.GlorotUniform(seed=seed),
                 use_bias=use_bias
         )])
@@ -107,9 +107,9 @@ class DecoderLSTM(tf.keras.layers.Layer):
         # The one step of LSTM computation.
         def loop_body(step, h, c, outputs):
             y = self.dense(h)  # prediction of word (embedding) by dense layer
-            h_next, c_next = self.cell(y, h, c)  # compute new h and c based on y and previous h, c
+            h_next, _ = self.cell(y, h, c)  # compute new h and c based on y and previous h, c
             outputs = outputs.write(step, y)  # remember predicted word
-            return step + 1, h_next, c_next, outputs
+            return step + 1, h_next, c_0, outputs
 
         # Outputs from LSTM (y predicted).
         outputs = tf.TensorArray(tf.float32, size=0, dynamic_size=True)

translator.py
@@ -12,13 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
-import numpy as np
+
 import tensorflow as tf
 
 from rnn.gru import DeepEncoderGRU
 from rnn.lstm import DecoderLSTM
-from gensim.utils import simple_preprocess
 
 
 @tf.keras.utils.register_keras_serializable()